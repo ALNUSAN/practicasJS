7d173b5638b423e4f82a010e2a1dac2bbd16c874
ALNUSAN <alfaaumnunez@gmail.com>
2023-08-15 10:32:50 -0500

    Terminado y ajustado ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,34 +1,14 @@
 /**Ejercicio 6: Decisión de beca
 Solicita al usuario datos personales como el nombre la edad y su pais tambien su departamento, el programa debe otorgar becas academicas a personas que solo sean de toronto canada, mayores de 18 */
 
-let nombre = prompt("Ingresa tu nombre");
-let edad = prompt("Ingresa tu edad");
-let pais = prompt("Ingresa tu país");
-let departamento = prompt("Ingresa el Departamento");
+let nombre = prompt("Ingresa tu nombre").toLocaleLowerCase();
+let edad = prompt("Ingresa tu edad").toLocaleLowerCase();
+let pais = prompt("Ingresa tu país").toLocaleLowerCase();
+let departamento = prompt("Ingresa el Departamento").toLocaleLowerCase();
 
-if (
-    nombre === null ||
-    nombre === "" ||
-    edad === null ||
-    edad === "" ||
-    pais === null ||
-    pais === "" ||
-    departamento === null ||
-    departamento === ""
-) {
-    alert("Datos no validos")
+if (pais ==="canada" || pais==="canadá" && departamento==="toronto" && edad>="18") {
+    alert("Felicidades cumples las condiciones para la Beca")
     
 } else {
-nombre = nombre.toLowerCase();
-edad = edad.toLowerCase();
-pais = pais.toLowerCase();
-departamento = departamento.toLowerCase();
-if (pais ==="canada" || pais ==="canadá" && departamento === "toronto" && edad >= 18) {
-    alert("Se te ha otorgado una beca")
-    
-} else {
-    alert("No cumples los requisitos para la beca")
-}
-
-    
+    alert("No cumples las condiciones para la beca")
 }
